fa4fdb90a42fac8fdb130973bd81aac6a7290528
Danyil Khokhulia <danyil.khokhulia@nure.ua>
2025-10-21 02:46:41 +0200

    Added Unit Of Work logic

---
+++ CryptexAPI/CryptexAPI/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using CryptexAPI.Repos.Interfaces;
+
+namespace CryptexAPI.UnitOfWork;
+
+public interface IUnitOfWork
+{
+    ICoinRepository CoinRepository { get; }
+    ISeedPhraseRepository SeedPhraseRepository { get; }
+    IUserRepository UserRepository { get; }
+    IWalletForMarketRepository WalletForMarketRepository { get; }
+    IWalletRepository WalletRepository { get; }
+    Task SaveChangesAsync();
+}
---
+++ CryptexAPI/CryptexAPI/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,53 @@
+﻿using CryptexAPI.Context;
+using CryptexAPI.Repos.Interfaces;
+
+namespace CryptexAPI.UnitOfWork;
+
+public class UnitOfWork : IUnitOfWork, IDisposable
+{
+    private readonly AppDbContext _dbContext;
+    private bool disposed = false;
+    public ICoinRepository CoinRepository { get; private set; }
+    public ISeedPhraseRepository SeedPhraseRepository { get; private set; }
+    public IUserRepository UserRepository { get; private set; }
+    public IWalletForMarketRepository WalletForMarketRepository { get; private set; }
+    public IWalletRepository WalletRepository { get; private set; }
+    public UnitOfWork(AppDbContext dbContext,
+        ICoinRepository coinRepository,
+        ISeedPhraseRepository seedPhraseRepository,
+        IUserRepository userRepository,
+        IWalletForMarketRepository walletForMarketRepository,
+        IWalletRepository walletRepository)
+    {
+        _dbContext = dbContext;
+        CoinRepository = coinRepository;
+        SeedPhraseRepository = seedPhraseRepository;
+        UserRepository = userRepository;
+        WalletForMarketRepository = walletForMarketRepository;
+        WalletRepository = walletRepository;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _dbContext.SaveChangesAsync();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposed)
+        {
+            if (disposing)
+            {
+                _dbContext.Dispose();
+            }
+        }
+
+        disposed = true;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+}
