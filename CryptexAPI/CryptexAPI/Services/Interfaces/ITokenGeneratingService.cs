e5e18ebc7919f546e89afbaac60f9e485a2ff0ff
Danyil Khokhulia <danyil.khokhulia@nure.ua>
2025-10-21 15:59:34 +0200

    Added rest of updates

---
+++ CryptexAPI/CryptexAPI/Services/Interfaces/ITokenGeneratingService.cs
@@ -0,0 +1,10 @@
+﻿using CryptexAPI.Models.Persons;
+using System.Security.Claims;
+
+namespace CryptexAPI.Services.Interfaces;
+
+public interface ITokenGeneratingService
+{
+    string GenerateToken(User user);
+    string GenerateToken(List<Claim> userClaims);
+}
