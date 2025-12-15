e85dc2e89bf13fc9ec30ff83dd13cfb4e0f882a6
Danyil Khokhulia <danyil.khokhulia@nure.ua>
2025-12-15 19:54:10 +0100

    Added new repositories

---
+++ CryptexAPI/CryptexAPI/Repos/TicketRepository.cs
@@ -0,0 +1,7 @@
+﻿using CryptexAPI.Context;
+using CryptexAPI.Models;
+using CryptexAPI.Repos.Interfaces;
+
+namespace CryptexAPI.Repos;
+
+public class TicketRepository(AppDbContext context) : BaseRepository<Ticket>(context), ITicketRepository;
