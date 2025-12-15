b58a6ff24b207e073d64772bde7e8c199e4e4584
Danyil Khokhulia <danyil.khokhulia@nure.ua>
2025-12-15 19:54:42 +0100

    Added new services

---
+++ CryptexAPI/CryptexAPI/Services/Interfaces/IMessageService.cs
@@ -0,0 +1,10 @@
+﻿using CryptexAPI.Models;
+
+namespace CryptexAPI.Services.Interfaces;
+
+public interface IMessageService
+{
+    Task<Message> CreateMessage(string messageValue, int author, int ticketId);
+
+    Task<List<Message>> GetChatHistoryOfTicket(int ticketId);
+}
