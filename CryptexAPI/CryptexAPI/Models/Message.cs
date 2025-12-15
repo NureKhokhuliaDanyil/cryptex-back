19459703314f92d108fa683089e88a692d879416
Danyil Khokhulia <danyil.khokhulia@nure.ua>
2025-12-15 19:54:01 +0100

    Added new entities

---
+++ CryptexAPI/CryptexAPI/Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using CryptexAPI.Models.Base;
+
+namespace CryptexAPI.Models;
+
+public class Message : BaseEntity
+{
+    public string Value { get; set; }
+    public int AuthorId { get; set; }
+    public int TicketId { get; set; }
+}
